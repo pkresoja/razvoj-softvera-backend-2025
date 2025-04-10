126fadb9782f2db79e288713e479038866695d87
Petar Kresoja <pkresoja@singidunum.ac.rs>
2025-04-10 13:29:47 +0200

    Add Vehicle CRUD operations

--- src/routes/vehicle.route.ts
+++ src/routes/vehicle.route.ts
@@ -13,3 +13,41 @@ VehicleRoute.get('/client/:id', async (req, res) => {
         errorReponse(res, e)
     }
 })
+
+VehicleRoute.get('/:id', async (req, res) => {
+    try {
+        const id = Number.parseInt(req.params.id)
+        res.json(await VehicleService.getVehicleById(id))
+    } catch (e: any) {
+        errorReponse(res, e)
+    }
+})
+
+VehicleRoute.post('/', async (req, res) => {
+    try {
+        await VehicleService.createVehicle(req.body)
+        res.status(204).send()
+    } catch (e: any) {
+        errorReponse(res, e)
+    }
+})
+
+VehicleRoute.put('/:id', async (req, res) => {
+    try {
+        const id = Number.parseInt(req.params.id)
+        await VehicleService.updateVehicle(id, req.body)
+        res.status(204).send()
+    } catch (e: any) {
+        errorReponse(res, e)
+    }
+})
+
+VehicleRoute.delete('/:id', async (req, res) => {
+    try {
+        const id = Number.parseInt(req.params.id)
+        await VehicleService.deleteVehicle(id)
+        res.status(204).send()
+    } catch (e: any) {
+        errorReponse(res, e)
+    }
+})
--- src/services/vehicle.service.ts
+++ src/services/vehicle.service.ts
@@ -51,4 +51,49 @@ export class VehicleService {
             }
         })
     }
+
+    static async getVehicleById(id: number) {
+        const data = await repo.findOne({
+            where: {
+                vehicleId: id,
+                client: {
+                    deletedAt: IsNull()
+                },
+                deletedAt: IsNull()
+            }
+        })
+
+        if (data == null)
+            throw new Error('VEHICLE_NOT_FOUND')
+
+        return data
+    }
+
+    static async createVehicle(model: Vehicle) {
+        await repo.save({
+            clientId: model.clientId,
+            modelId: model.modelId,
+            regPlate: model.regPlate,
+            vin: model.vin,
+            year: model.year,
+            createdAt: new Date()
+        })
+    }
+
+    static async updateVehicle(id: number, model: Vehicle) {
+        const vehicle = await this.getVehicleById(id);
+        vehicle.clientId = model.clientId
+        vehicle.modelId = model.modelId
+        vehicle.regPlate = model.regPlate
+        vehicle.vin = model.vin
+        vehicle.year = model.year
+        vehicle.updatedAt = new Date()
+        await repo.save(vehicle)
+    }
+
+    static async deleteVehicle(id: number) {
+        const vehicle = await this.getVehicleById(id);
+        vehicle.deletedAt = new Date()
+        await repo.save(vehicle)
+    }
 }
